14b89b472e09785fd0f0082d0689f026c2db2f9c
DeanDoyle1502 <20096595@mail.wit.ie>
2023-09-27 14:49:42 +0100

    fixing no used var Lab 2

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -7,7 +7,7 @@ function App() {
     tasks: [
       { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
       { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" },
+      { id: 3, title: "Tidy up", description: "Tidy away things", deadline: "Today" },
       { id: 4, title: "Wash car", description: "Clean Car", deadline: "Today" },
       { id: 5, title: "Go to College", description: "Attend College", deadline: "Morning"}
     ]
