23ca4ecd705de4f60d2e34a05ea2d85a9ecb3534
DeanDoyle1502 <20096595@mail.wit.ie>
2023-09-27 19:46:29 +0100

    Completed Week 2 Lab

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -5,8 +5,8 @@ const Task = (props) => {
     return (
         <div className="card">
             <p className="title">{props.title}</p>
-            <p  className='description'>{props.description}</p>
-            <p className="deadline">{props.deadline}</p>
+            <p className='description'>{props.description}</p>
+            <p> Due:  {props.deadline}</p>
             <p>{props.children}</p>
         </div>
     )
