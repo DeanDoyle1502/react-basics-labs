5adcb493b1384aebba4cc5e1f1ad3777208c2bcf
DeanDoyle1502 <20096595@mail.wit.ie>
2023-10-08 18:32:56 +0100

    Week 3 Exercises done

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import React, { useState } from 'react';
 import AddTaskForm from './components/Form';
 import { v4 as uuidv4 } from 'uuid';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+
 
 function App() {
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
-      { id: 3, title: "Tidy up", deadline: "Today", done: false},
-      { id: 4, title: "Wash car", description: "Clean Car", deadline: "Today", done: false },
-      { id: 5, title: "Go to College", description: "Attend College", deadline: "Morning", done: false}
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "Medium" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "Low" },
+      { id: 3, title: "Tidy up", deadline: "Today", done: false, priority: "Low"},
+      { id: 4, title: "Wash car", description: "Clean Car", deadline: "Today", done: false, priority: "High" },
+      { id: 5, title: "Go to College", description: "Attend College", deadline: "Morning", done: false, priority: "Low"}
     ]
   });
   
@@ -41,8 +45,12 @@ function App() {
       case "deadline":
           form.deadline = event.target.value;
           break;
+      case "priority":
+          form.priority = event.target.value;
+          break;    
       default:
           form = formState;
+      
     }
     setFormState(form);
     console.log(formState);
@@ -68,24 +76,64 @@ function App() {
 
   return (
     <div className="container">
-      <h1>Tasky</h1>
-      {taskState.tasks.map((task, index) => ( 
-        
-        <Task 
-        title={task.title}
-        description={task.description}
-        deadline={task.deadline}
-        key={task.id}
-        done={task.done}
-        markDone={() => doneHandler(index)}
-        deleteTask = {() => deleteHandler(index)}  
-    />
-  ))} 
+       {/* App Header */}
+       <Container component="main">
+        <Typography
+          component="h1"
+          variant="h2"
+          align="center"
+          gutterBottom
+          sx = {{
+            backgroundColor: 'gray',
+            textAlign: 'center',
+            color: 'white',
+            padding: '20px',
+            margin: '20px 0 40px 0',
+            borderRadius: '4px'
+          }}
+        >
+          Tasky
+        </Typography>
+      </Container>
+      {/* End App Header */}
 
-<AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+       {/* Task Card Grid */}
+       <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-top" justifyContent="center">
+          {taskState.tasks.map((task, index) => (
+                <Task 
+                title={task.title}
+                description={task.description}
+                deadline={task.deadline}
+                done={task.done}
+                key={task.id}
+                markDone = {() => doneHandler(index)}
+                deleteTask = {() => deleteHandler(index)}
+                priority={task.priority}
+              />
+          ))}
+        </Grid>
+      </Container>
+      {/* End Task Card Grid */}
+
+ {/* Footer - Add Task Form */}
+ <Container
+        component="footer"
+        sx={{
+          borderTop: (theme) => `1px solid ${theme.palette.divider}`,
+          my: 6,
+          py: 6,
+        }}
+      >
+        <Grid container justifyContent="center">
+          <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
+        </Grid>
+      </Container>
+      {/* End Footer */}
 
 
     </div>
+    
   );
 }
 
